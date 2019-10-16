950c5af17d855b83c55441de1b94952637f2fa94
Cade Christiansen <cade.christiansen@niceincontact.com>
2019-10-16 16:28:01 -0600

    Created the SodaRush schema and added the Store table to that schema.

---
+++ MasterDB/Schema Objects/Schemas/SodaRush/Tables/Store.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [SodaRush].[Store]
+(
+	[StoreId] INT NOT NULL PRIMARY KEY,
+	[StoreName] NVARCHAR(200) NOT NULL,
+	[StreetAddress] NVARCHAR(500),
+	[City] NVARCHAR(100),
+	[State] NVARCHAR(2),
+	[ZipCode] SMALLINT CONSTRAINT MaxConstraint_StoreZipCode CHECK(ZipCode > 0 AND ZipCode < 100000)
+)
