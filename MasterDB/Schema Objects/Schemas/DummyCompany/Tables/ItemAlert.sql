948f36e6002735b42235664b23d1504d75a83018
Cade Christiansen <cade.christiansen@niceincontact.com>
2019-11-04 13:14:59 -0700

    Added the ItemAlert table

---
+++ MasterDB/Schema Objects/Schemas/DummyCompany/Tables/ItemAlert.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [DummyCompany].[ItemAlert]
+(
+	[ItemAlertId] INT NOT NULL PRIMARY KEY,
+	[ItemId] INT CONSTRAINT [FK_ItemAlert_ItemID] FOREIGN KEY REFERENCES [DummyCompany].[Item](ItemId) NOT FOR REPLICATION,
+	[StoreId] INT CONSTRAINT [FK_ItemAlert_StoreID] FOREIGN KEY REFERENCES [DummyCompany].[Store](StoreId) NOT FOR REPLICATION,
+	[ModerateLevel] FLOAT,
+	[UrgentLevel] FLOAT
+)
---
+++ MasterDB/Schema Objects/Schemas/SodaRush/Tables/ItemAlert.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [SodaRush].[ItemAlert]
+(
+	[ItemAlertId] INT NOT NULL PRIMARY KEY,
+	[ItemId] INT CONSTRAINT [FK_ItemAlert_ItemID] FOREIGN KEY REFERENCES [SodaRush].[Item](ItemId) NOT FOR REPLICATION,
+	[StoreId] INT CONSTRAINT [FK_ItemAlert_StoreID] FOREIGN KEY REFERENCES [SodaRush].[Store](StoreId) NOT FOR REPLICATION,
+	[ModerateLevel] FLOAT,
+	[UrgentLevel] FLOAT
+)
